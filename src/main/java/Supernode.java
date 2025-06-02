ef6826c8fd0dea9c1ca16747537b0a3cf478e493
DerekNguyenn <dereknguyenbusiness@gmail.com>
2025-06-02 11:59:22 -0700

    Create Supernode subclass

---
+++ src/main/java/Supernode.java
@@ -0,0 +1,15 @@
+public class Supernode extends PeerNode {
+    public Supernode(int id, double x, double y, int totalChunks) {
+        super(id, x, y, totalChunks);
+        this.uploadSpeed *= 2;
+        this.downloadSpeed *= 1.5;
+    }
+
+    public String getNodeType() {
+        return "Supernode";
+    }
+
+    public boolean canAcceptMoreConnections() {
+        return this.connections.size() < 12;
+    }
+}
