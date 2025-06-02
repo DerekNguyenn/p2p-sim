1cf347d80ec56378ebeb0cf652bb52315551ab87
DerekNguyenn <dereknguyenbusiness@gmail.com>
2025-06-02 11:52:32 -0700

    Create Seeder and Leecher subclass

---
+++ src/main/java/Leecher.java
@@ -0,0 +1,18 @@
+public class Leecher extends PeerNode {
+    public Leecher(int id, double x, double y, int totalChunks) {
+        super(id, x, y, totalChunks);
+    }
+
+    public String getNodeType() {
+        return "Leecher";
+    }
+
+    public void downloadFrom(PeerNode peer) {
+        for (int chunk : peer.getOwnedChunks()) {
+            if (!this.ownedChunks.contains(chunk)) {
+                this.receiveChunk(chunk);
+                break; // One chunk per tick
+            }
+        }
+    }
+}
---
+++ src/main/java/Seeder.java
@@ -0,0 +1,13 @@
+public class Seeder extends PeerNode {
+    public Seeder(int id, double x, double y, int totalChunks) {
+        super(id, x, y, totalChunks);
+
+        for (int i = 0; i < totalChunks; i++) {
+            ownedChunks.add(i);
+        }
+    }
+
+    public String getNodeType() {
+        return "Seeder";
+    }
+}
