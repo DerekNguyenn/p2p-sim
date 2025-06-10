c8dfe0864bc0342da5dcaf9cb1bcc0fcc8a96756
DerekNguyenn <dereknguyenbusiness@gmail.com>
2025-06-10 08:52:58 -0700

    Create new constants class for units of measure

---
+++ src/main/java/org/derekn/p2pSim/Constants.java
@@ -0,0 +1,8 @@
+package org.derekn.p2pSim;
+
+public class Constants {
+    public static final int CHUNK_SIZE = (int) Math.pow(1024, 2);
+    public static final long GB = 1_073_741_824L;
+    public static final long MB = 1_048_576L;
+    public static final long KB = 1_024L;
+}
