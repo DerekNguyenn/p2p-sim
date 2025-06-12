d1c39b699f4135975083a56b8f3897c4eb7d35da
DerekNguyenn <dereknguyenbusiness@gmail.com>
2025-06-12 08:42:17 -0700

    Import javafx-media

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.derekn.p2pSim {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     opens org.derekn.p2pSim to javafx.fxml;
     exports org.derekn.p2pSim;
